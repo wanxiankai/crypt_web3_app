e8cd04798b99ed5959f2ef4e98e6a5f11b6f9fdc
wanxiankai <wanxiankai@126.com>
2024-04-09 23:22:21 +0800

    feat: complete the loader component

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-
 export default function Loader() {
   return (
-    <div>Loader</div>
+    <div className="flex justify-center items-center p-3">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-red-700"/>
+    </div>
   )
 }
