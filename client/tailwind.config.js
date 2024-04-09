cddc29e9f393c4ade54cafebbca06accd26d1f4e
wanxiankai <wanxiankai@126.com>
2024-04-09 23:22:40 +0800

    feat: change tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,9 +4,38 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  mode: "jit",
+  darkMode: false,
   theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+
+  plugins: ["@tailwindcss/forms"],
 }
 
