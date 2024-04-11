eaaaecb5fed839893103bf6cb8547fdbbb49c1ef
wanxiankai <wanxiankai@126.com>
2024-04-11 09:25:07 +0800

    feat: deploy the smart contract on test network

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './transactions.json'
+
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x35c708ceC95f14cf594e3F17D78B5b5555D27EF0';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    Sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/hV6FR6aTSNdcyh87PCD9oo0T2PKQdCvv",
+      accounts:['6f4a8c331ab13fedfee38ccd83efba77558beab1e39dc8dc6f4945e9bd6ef7fa']
+    }
+  }
 };
